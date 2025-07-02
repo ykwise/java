7f08eebaf01f9b3c25f63ea5a4c20d44a8d75a85
ykwise <yeswanthdevarasetti@gmail.com>
2025-07-02 14:53:10 +0530

    java_program

---
+++ program2.java
@@ -0,0 +1,7 @@
+public class program2 {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            System.out.println("Square  of " + i +":"+i*i);
+        }
+    }
+}
